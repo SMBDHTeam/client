ce5d2beae1fc8890848ab581a20a2ec7f13d6fc0
YangJinHyeok <101380919+YangJinHyeok@users.noreply.github.com>
2026-07-14 22:45:53 +0900

    feat : 커뮤니티 준비 화면

---
+++ src/app/(app)/community/page.tsx
@@ -0,0 +1,12 @@
+import AppHeader from "@/components/AppHeader";
+
+export default function CommunityPage() {
+  return (
+    <div className="flex flex-1 flex-col">
+      <AppHeader title="커뮤니티" />
+      <div className="grid flex-1 place-items-center px-5 text-center text-zinc-400">
+        준비 중인 화면이에요
+      </div>
+    </div>
+  );
+}
