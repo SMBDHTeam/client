7a28a209afa131633728b992ac029306045455c0
YangJinHyeok <101380919+YangJinHyeok@users.noreply.github.com>
2026-07-14 22:46:02 +0900

    feat : 내정보 준비 화면

---
+++ src/app/(app)/profile/page.tsx
@@ -0,0 +1,12 @@
+import AppHeader from "@/components/AppHeader";
+
+export default function ProfilePage() {
+  return (
+    <div className="flex flex-1 flex-col">
+      <AppHeader title="내정보" />
+      <div className="grid flex-1 place-items-center px-5 text-center text-zinc-400">
+        준비 중인 화면이에요
+      </div>
+    </div>
+  );
+}
